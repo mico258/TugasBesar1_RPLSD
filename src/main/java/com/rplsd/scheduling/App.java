a7e527e72d9966d0b89cbd32d67c3c26f5b5d7a6
Tasya <agathastellatasya@gmail.com>
2018-11-21 04:22:45 +0700

    Try to fix dependencies.

---
+++ src/main/java/com/rplsd/scheduling/App.java
@@ -0,0 +1,22 @@
+package com.rplsd.scheduling;
+
+import java.io.PrintWriter;
+
+import org.antlr.v4.runtime.*;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        ANTLRInputStream inputStream = new ANTLRInputStream(
+                "I would like to [b]emphasize[/b] this and [u]underline [b]that[/b][/u]. " +
+                        "Let's not forget to quote: [quote author=\"John\"]You're wrong![/quote]");
+        scheduleLexer scheduleLexer = new scheduleLexer(inputStream);
+        CommonTokenStream commonTokenStream = new CommonTokenStream(scheduleLexer);
+        scheduleParser scheduleParser = new scheduleParser(commonTokenStream);
+
+        scheduleParser.FiturContext fileContext = scheduleParser.getContext();
+        scheduleVisitor visitor = new scheduleVisitor(System.out);
+        visitor.visit(scheduleLexer);
+    }
+}
