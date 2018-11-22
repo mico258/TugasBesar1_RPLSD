025582a61fcb93477fd6630001d385ce142b6eb4
rizkiihza <rizki.ihza@gmail.com>
2018-11-22 16:43:54 +0700

    matrix kelar

--- src/main/java/com/rplsd/scheduling/ruangan/Ruangan.java
+++ src/main/java/com/rplsd/scheduling/ruangan/Ruangan.java
@@ -8,6 +8,8 @@ public class Ruangan {
     public Integer capacity;
     public Integer facility;
 
+    public boolean[][] used;
+
     public Ruangan(scheduleParser.RuanganContext rc) {
         ArrayList<String> temp = new ArrayList<>();
         for (int i = 0; i < rc.NUMBER().size(); i++) {
@@ -16,6 +18,13 @@ public class Ruangan {
         this.name = temp.get(0);
         this.capacity = Integer.parseInt(temp.get(1));
         this.facility = Integer.parseInt(temp.get(2));
+
+        used = new boolean[6][12];
+        for (int day = 1; day <= 5; day++) {
+            for (int hour = 1; hour <= 11; hour++) {
+                used[day][hour] = false;
+            }
+        }
     }
 
     public String toString() {
